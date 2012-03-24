550ce4ffeae5aad9b792cbc8324f40e73dd9f202
Mark Markarian <mark@interzonedev.com>
2012-03-24 11:54:57 -0700

    Upped the version to 1.0.3-SNAPSHOT.
    Altered to build final jar with dependencies.

--- src/main/java/com/interzonedev/herokusupport/webserver/WebServerUtils.java
+++ src/main/java/com/interzonedev/herokusupport/webserver/WebServerUtils.java
@@ -1,12 +1,18 @@
 package com.interzonedev.herokusupport.webserver;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class WebServerUtils {
+	private static Log log = LogFactory.getLog(WebServerUtils.class);
+
 	public static int getPortFromEnv(int defaultWebPort) {
 		int webPort = defaultWebPort;
 
 		String webPortFromEnv = System.getenv("PORT");
+		log.info("getPortFromEnv: webPortFromEnv = " + webPortFromEnv);
+
 		if (StringUtils.isNotBlank(webPortFromEnv)) {
 			webPort = Integer.valueOf(webPortFromEnv);
 		}
