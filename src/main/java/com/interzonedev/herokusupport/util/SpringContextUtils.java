93578cc9ab831b20ee48a8600b0b915fc0ab1ccf
Mark Markarian <mark@interzonedev.com>
2012-05-04 00:50:04 -0700

    Added the SpringContextUtils utility class for managing Spring application contexts.
    Added testing capabilities and an integration test for SpringContextUtils.

---
+++ src/main/java/com/interzonedev/herokusupport/util/SpringContextUtils.java
@@ -0,0 +1,49 @@
+package com.interzonedev.herokusupport.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Assert;
+
+public class SpringContextUtils {
+
+	private static final Map<String[], ApplicationContext> applicationContexts = new HashMap<String[], ApplicationContext>();
+
+	public static ApplicationContext getApplicationContext(String[] configLocations) {
+
+		Assert.notNull(configLocations, "getApplicationContext: The config locations must be set");
+		Assert.notEmpty(configLocations, "getApplicationContext: The config locations must not be empty");
+
+		ApplicationContext applicationContext = null;
+
+		for (String[] aConfigLocations : applicationContexts.keySet()) {
+			if (ArrayUtils.isEquals(configLocations, aConfigLocations)) {
+				applicationContext = applicationContexts.get(aConfigLocations);
+			}
+		}
+
+		if (null == applicationContext) {
+			applicationContext = new ClassPathXmlApplicationContext(configLocations);
+			applicationContexts.put(configLocations, applicationContext);
+		}
+
+		return applicationContext;
+
+	}
+
+	public static Object getBean(String[] configLocations, String beanName) {
+
+		Assert.notNull(configLocations, "getBean: The config locations must be set");
+		Assert.notEmpty(configLocations, "getBean: The config locations must not be empty");
+		Assert.hasText(beanName, "getBean: The bean name must be set");
+
+		ApplicationContext applicationContext = getApplicationContext(configLocations);
+		Object bean = applicationContext.getBean(beanName);
+		return bean;
+
+	}
+
+}
---
+++ src/test/java/com/interzonedev/herokusupport/util/TestBean.java
@@ -0,0 +1,46 @@
+package com.interzonedev.herokusupport.util;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+public class TestBean {
+
+	private String value;
+
+	public TestBean(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).append("value", getValue()).toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37).append(getValue()).toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof TestBean)) {
+			return false;
+		}
+
+		TestBean that = (TestBean) obj;
+
+		boolean equals = new EqualsBuilder().append(getValue(), that.getValue()).isEquals();
+
+		return equals;
+	}
+
+}
